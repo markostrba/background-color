901596b42a613d69f785284410da864927e5f70f
markostrba <mark3strba@gmail.com>
2023-12-30 09:48:37 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+//Question 2: Write a javascript function that takes an array of numbers and a target number.
+//The function should find two different numbers in the array that, when added together, give the target number.
+// For example: answer([1,2,3], 4)should return [1,3]
+
+const answer = (arr, target) => {
+  return arr.filter((e, i) => );
+};
+
+console.log("in:", [1, 2, 3], 4);
+console.log("out:", answer([1, 2, 3], 4));
